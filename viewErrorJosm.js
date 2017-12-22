c868042492e3fc613025ed4b4c0c84179ffb3683
RubenGeo <34537157+RubenGeo@users.noreply.github.com>
2017-12-22 14:53:42 +0100

    Add files via upload

--- viewErrorJosm.js
+++ viewErrorJosm.js
@@ -1,3 +1,6 @@
+// Firs open manually open a background imagery you can use digital globe premium,
+// but it may also be useful to open 
+
 var api = require("josm/api").Api;
 var nbuilder = require("josm/builder").NodeBuilder;
 var SimlePrimitiveId = org.openstreetmap.josm.data.osm.SimplePrimitiveId;
@@ -58,29 +61,22 @@ function select_way_with_id(wayOfInterest){
 
 
 function download_box_from_lat_lon(lat, lon){
-    var latMin = lat - 0.002
-    var lonMin = lon - 0.002
-    var latMax = lat + 0.002
-    var lonMax = lon + 0.002
+    var latMin = lat - 0.01
+    var lonMin = lon - 0.01
+    var latMax = lat + 0.01
+    var lonMax = lon + 0.01
 
-
+    
     var ds = api.downloadArea({
         min: {lat: latMin, lon: lonMin},
         max: {lat: latMax, lon: lonMax}
     });
-
-    josm.layers.addDataLayer({ds: ds, name: "PotentialError"}); 
+    return ds
+    
 };
 
 
 
-var wayID = 24951029
-
-var latStart = -14.2956651 
-var lonStart = 33.983665
-
-var latEnd = -14.2807553
-var lonEnd = 33.787281
 
 function PotentialError (wayID, startPoint, endPoint) {
     this.wayID = wayID;
@@ -90,21 +86,70 @@ function PotentialError (wayID, startPoint, endPoint) {
 }
  
 PotentialError.prototype.show_error_area = function() {    
-    download_box_from_lat_lon(this.startPoint[0], this.startPoint[1])
-    download_box_from_lat_lon(this.endPoint[0], this.endPoint[1])
+    var ds1 =  download_box_from_lat_lon(this.startPoint[0], this.startPoint[1])
+    var ds2 = download_box_from_lat_lon(this.endPoint[0], this.endPoint[1])
+
+    
+    //ds1.each(doeIets)
+    
+/*     function doeIets(p) {
+        console.println(p)
+        console.println('Hoi')
+        ds2.add(p)
+        console.println('Doei')
+    } */
+
+
+    
+    josm.layers.addDataLayer({ds: ds1, name: "PotentialError"});
+    josm.layers.addDataLayer({ds: ds1, name: "PotentialError"});
+
     select_way_with_id(this.wayID)
 };
 
-    
+
+function loop_through_errors(errorList){
+    errorList.forEach(function(wayError) {
+        console.println(wayError.wayID);
+        
+        wayError.show_error_area()
+        
+        josm.alert(wayError.wayID)
+        
+        var layers = require("josm/layers");
+        layers.remove("PotentialError");
+        layers.remove("PotentialError");
+    });
+};
+
+var wayID = 24951029
+
+var latStart = -14.2956651 
+var lonStart = 33.983665
+
+var latEnd = -14.2807553
+var lonEnd = 33.787281
+
 var testWay = new PotentialError(wayID, [latStart, lonStart], [latEnd, lonEnd])
 
-testWay.show_error_area()
+var wayID = 28972086
+
+var latStart = -14.1708296
+var lonStart = 33.6405743
+
+var latEnd = -14.1394315
+var lonEnd = 33.660154
+var testWay1 = new PotentialError(wayID, [latStart, lonStart], [latEnd, lonEnd])
+
+    
+var listOfWays = [testWay, testWay1]
+
+loop_through_errors(listOfWays)
 
 
-/* download_box_from_lat_lon(latStart, lonStart)
-download_box_from_lat_lon(latEnd, lonEnd)
-select_way_with_id(wayID) */
 
-console.println(l)
+
+
+
 console.println("Klaar " )
 
